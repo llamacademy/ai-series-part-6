48f5e288d5d62ab1639e15ec3b6e338ad426df89
Chris Kurhan <chris.kurhan@hotmail.com>
2021-02-24 06:43:29 -0600

    Initial Commit

---
+++ Assets/Scripts/AttackRadius.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class AttackRadius : MonoBehaviour
+{
+    public SphereCollider Collider;
+    private List<IDamageable> Damageables = new List<IDamageable>();
+    public int Damage = 10;
+    public float AttackDelay = 0.5f;
+    public delegate void AttackEvent(IDamageable Target);
+    public AttackEvent OnAttack;
+    private Coroutine AttackCoroutine;
+
+    private void Awake()
+    {
+        Collider = GetComponent<SphereCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamageable damageable = other.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            Damageables.Add(damageable);
+
+            if (AttackCoroutine == null)
+            {
+                AttackCoroutine = StartCoroutine(Attack());
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        IDamageable damageable = other.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            Damageables.Remove(damageable);
+            if (Damageables.Count == 0)
+            {
+                StopCoroutine(AttackCoroutine);
+                AttackCoroutine = null;
+            }
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        WaitForSeconds Wait = new WaitForSeconds(AttackDelay);
+
+        yield return Wait;
+
+        IDamageable closestDamageable = null;
+        float closestDistance = float.MaxValue;
+
+        while (Damageables.Count > 0)
+        {
+            for (int i = 0; i < Damageables.Count; i++)
+            {
+                Transform damageableTransform = Damageables[i].GetTransform();
+                float distance = Vector3.Distance(transform.position, damageableTransform.position);
+
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestDamageable = Damageables[i];
+                }
+            }
+
+            if (closestDamageable != null)
+            {
+                OnAttack?.Invoke(closestDamageable);
+                closestDamageable.TakeDamage(Damage);
+            }
+
+            closestDamageable = null;
+            closestDistance = float.MaxValue;
+
+            yield return Wait;
+
+            Damageables.RemoveAll(DisabledDamageables);
+        }
+
+        AttackCoroutine = null;
+    }
+
+    private bool DisabledDamageables(IDamageable Damageable)
+    {
+        return Damageable != null && !Damageable.GetTransform().gameObject.activeSelf;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Navmesh AI Tutorial 6 - Adding Ranged Attacking Enemies
+The below video is the tutorial that **ENDS** where this project is. If you would like to follow along, check out the [5 Creating Different Enemy Types](https://github.com/llamacademy/ai-series-part-5) 
+
+[![Youtube Tutorial](./Video%20Screenshot.png)](https://youtu.be/Aee01YxQIsw&ref=github)
+
+**IMMEDIATELY AFTER CHECKING THIS OUT** IMPORT THE [UNITY CHAN MODEL](https://assetstore.unity.com/packages/3d/characters/unity-chan-model-18705) FROM THE UNITY ASSET STORE or you will be missing the model! (it's free)
+
+In this project we have:
+1. Simple click to move.
+2. An enemy that follows the player
+3. NavMeshLinks to allow the player and enemy to jump from one platform to another, and on top of some walls.
+4. AgentLinkMover to control how NavMeshAgents will traverse NavMeshLinks
+5. Animated 3D Model based on NavMeshAgent's movement
+6. Dynamically spawned enemies at random points on the NavMesh
+7. 2 Enemy types, configured via a ScriptableObject, that path differently based on Agent Configuration
+8. Enemies and Player attack the other when they near each other, until dead.
+
+## Requirements
+* Requires Unity 2019.4 LTS or higher. 
+* Utilizes the [Navmesh Components](https://github.com/Unity-Technologies/NavMeshComponents) from Unity's Github.
+* [Unity-Chan Model](https://assetstore.unity.com/packages/3d/characters/unity-chan-model-18705) from the Unity Asset Store (it's free)
